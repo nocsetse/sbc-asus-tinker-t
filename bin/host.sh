0f2c8e72dad30c5e3c51f3123fe1e21e5526eb1e
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-05-07 13:56:11 -0400

    testing python

---
+++ bin/host.sh
@@ -0,0 +1,4 @@
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+sudo apt-get update
+sudo apt-get install edgetpu-compiler

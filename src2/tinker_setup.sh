6161acd5590b7274c1e47f944f959c81926d9dc6
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-12-27 22:14:16 -0500

    update python build

--- src2/tinker_setup.sh
+++ src2/tinker_setup.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
+
+# 
 # devsecfranklin@duck.com
+# Dec 27 2022
+#
 
 sudo apt-get update
-sudo apt-get install cmake gcc g++  libxml2 libxml2-* leveldb*
+sudo apt-get install cmake gcc g++  libxml2 libxml2-* leveldb* git
 sudo apt-get install python3-setuptools python3-dev
 
-#import nltk
-#nltk.download()
-#quit()
-#sudo python -m textblob.download_corpora
-
+python3 -m textblob.download_corpora
+python3 -m spacy.en.download all
 
 function tensorFlow() {
   get https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.0.1/tensorflow-1.0.1...
-    sudo pip3 install tensorflow-1.0.1-cp34-cp34m-linux_arm7l.whl
+  python3 -m pip install tensorflow-1.0.1-cp34-cp34m-linux_arm7l.whl
 }
-
-function py27 () {
-sudo pip uninstall mock
-sudo pip install mock
-}
-
 
 function py34() {
-sudo pip3 uninstall mock
-sudo pip3 install mock
-sudo apt-get install git
-git clone https://github.com/tensorflow/tensorflow.git
+  python3 -m textblob.download_corpora
+  python3 -m spacy.en.download all
+  python3 -m pip uninstall mock
+  python3 -m pip install mock
 }
 
 function paho_for_mqtt() {
-pip install paho-mqtt 
+  python3 -m pip install paho-mqtt 
 }
 
 function main() {
+  git clone https://github.com/tensorflow/tensorflow.git
   tensorFlow
   py34
   paho_for_mqtt
